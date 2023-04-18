7125df87dd105becb60f103eb811129cdc1398da
Stephen Bolton <stephen@bitsonthemind.com>
2023-04-18 01:48:37 -0400

    Add deps needed for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,29 @@
 defmodule Childsplay.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/skbolton/childsplay"
+  @version "0.1.0"
+
   def project do
     [
       app: :childsplay,
-      version: "0.1.0",
-      description: "Helpers for building a Supervisor's children",
+      version: @version,
       elixir: "~> 1.14",
+
+      # Hex
+      description: "Helpers for building a Supervisor's children",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Stephen Bolton"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url},
+      files: ~w(.formatter.exs mix.exs README.md lib)
     ]
   end
 
@@ -22,7 +37,8 @@ defmodule Childsplay.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
